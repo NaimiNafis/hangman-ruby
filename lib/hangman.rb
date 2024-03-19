0bccf82bb5d4a6d259b4c5cdf25b97ed009db580
MeeSup <126528516+MeeSup@users.noreply.github.com>
2024-03-19 18:43:53 +0800

    Add method for check dictionary Add constant for MAX_GUESSES count Seperate main game logic with other methods Add method to update displayed word based on correct guess Refactor draw_hangman method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,108 +1,71 @@
 FILENAME = 'google-10000-english-no-swears.txt'
-dictionary = []
+MAX_GUESSES = 6
 
-if File.exist?(FILENAME)
-  dictionary = File.readlines(FILENAME).map(&:chomp)
-else
-  puts "Dictionary file not found."
-  exit
-end
-
-def draw_hangman(guesses_left)
-  case guesses_left
-  when 6
-    puts "-------"
-    puts "|     |"
-    puts "|"
-    puts "|"
-    puts "|"
-    puts "|"
-    puts "|"
-  when 5
-    puts "-------"
-    puts "|     |"
-    puts "|     O"
-    puts "|"
-    puts "|"
-    puts "|"
-    puts "|"
-  when 4
-    puts "-------"
-    puts "|     |"
-    puts "|     O"
-    puts "|     |"
-    puts "|"
-    puts "|"
-    puts "|"
-  when 3
-    puts "-------"
-    puts "|     |"
-    puts "|     O"
-    puts "|    /|"
-    puts "|"
-    puts "|"
-    puts "|"
-  when 2
-    puts "-------"
-    puts "|     |"
-    puts "|     O"
-    puts "|    /|\\"
-    puts "|"
-    puts "|"
-    puts "|"
-  when 1
-    puts "-------"
-    puts "|     |"
-    puts "|     O"
-    puts "|    /|\\"
-    puts "|    /"
-    puts "|"
-    puts "|"
-  when 0
-    puts "-------"
-    puts "|     |"
-    puts "|     O"
-    puts "|    /|\\"
-    puts "|    / \\"
-    puts "|"
-    puts "|"
-    puts "Game Over!"
-  end
-end
-
-secret_word = dictionary.sample
-puts secret_word
-display_word = "_" * secret_word.length
-guess_count = 6
-
-puts "Welcome to the hangman game!"
-
-while guess_count > 0
-  puts "\nGuesses left: #{guess_count}"
-  puts "Word: #{display_word}"
-  print "Please enter 1 alphabet: "
-  guess = gets.chomp.downcase
-
-  unless guess.match?(/\A[a-z]\z/)
-    puts "Please enter only one alphabet character."
-    next
-  end
-
-  if secret_word.include?(guess)
-    puts "You are correct!"
-    secret_word.chars.each_with_index do |char, index|
-      display_word[index] = char if char == guess
-    end
+def load_dictionary(filename)
+  if File.exist?(filename)
+    File.readlines(filename).map(&:chomp)
   else
-    puts "Incorrect guess."
-    draw_hangman(guess_count)
-    guess_count -= 1
-  end
-
-  if display_word == secret_word
-    puts "Congratulations, you won! The word was '#{secret_word}'."
+    puts "Dictionary file not found."
     exit
   end
 end
 
-puts "Game over! The word was '#{secret_word}'."
+def draw_hangman(guesses_left)
+  stages = [
+    ["  +---+", "  |   |", "      |", "      |", "      |", "      |", "========="],
+    ["  +---+", "  |   |", "  O   |", "      |", "      |", "      |", "========="],
+    ["  +---+", "  |   |", "  O   |", "  |   |", "      |", "      |", "========="],
+    ["  +---+", "  |   |", "  O   |", " /|   |", "      |", "      |", "========="],
+    ["  +---+", "  |   |", "  O   |", " /|\\  |", "      |", "      |", "========="],
+    ["  +---+", "  |   |", "  O   |", " /|\\  |", " /    |", "      |", "========="],
+    ["  +---+", "  |   |", "  O   |", " /|\\  |", " / \\  |", "      |", "Game Over!"]
+  ]
+
+  puts stages[MAX_GUESSES - guesses_left]
+end
+
+def update_display_word(secret_word, display_word, guess)
+  secret_word.chars.each_with_index do |char, index|
+    display_word[index] = char if char == guess
+  end
+end
+
+def play_hangman(dictionary)
+  secret_word = dictionary.sample
+  display_word = "_" * secret_word.length
+  guess_count = MAX_GUESSES
+
+  puts "Welcome to the hangman game!"
+  puts secret_word # For debugging purposes
+
+  while guess_count > 0
+    puts "\nGuesses left: #{guess_count}"
+    puts "Word: #{display_word}"
+    print "Please enter 1 alphabet: "
+    guess = gets.chomp.downcase
+
+    unless guess.match?(/\A[a-z]\z/)
+      puts "Please enter only one alphabet character."
+      next
+    end
+
+    if secret_word.include?(guess)
+      puts "You are correct!"
+      update_display_word(secret_word, display_word, guess)
+    else
+      puts "Incorrect guess."
+      draw_hangman(guess_count)
+      guess_count -= 1
+    end
+
+    if display_word == secret_word
+      puts "Congratulations, you won! The word was '#{secret_word}'."
+      exit
+    end
+  end
+
+  puts "Game over! The word was '#{secret_word}'."
+end
+
+dictionary = load_dictionary(FILENAME)
+play_hangman(dictionary)
